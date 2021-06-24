fc9154f04ba4af71e0e63896663eec81c7219719
Hayden Matz <haydenmatz97@gmail.com>
2021-06-24 14:48:32 -0500

    download functionality

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const axios = require('axios').default;
+const clip_url = 'https://clips.twitch.tv/SeductiveArtsyShingleANELE-z3xpTO8yfxxzsx04';
 
+let username = window.location.pathname
+let endpoint1 = `https://api.twitch.tv/kraken/clips/top?channel=${username.substring(1)}&trending=true&limit=6`;
+let endpoint2 = `https://api.twitch.tv/helix/users?login=${username.substring(1)}`;
 const videosList = document.getElementById('videosList')
 const messages = [];
+const downloadLinks = [];
+
 
 ComfyJS.onChat = ( user, message, flags, self, extra ) => {
     updateMessages(message);
@@ -18,21 +24,14 @@ const updateMessages = (text) => {
     node.appendChild(textnode);
     document.getElementById("messagesList").appendChild(node);
 }
-//attempt to reverse scroll
 
-
-let username = window.location.pathname
-let endpoint1 = `https://api.twitch.tv/kraken/clips/top?channel=${username.substring(1)}&trending=true&limit=6`;
-let endpoint2 = `https://api.twitch.tv/helix/users?login=${username.substring(1)}`;
 function getClips () { 
-  console.log("USER", username);
   axios.get(endpoint1, {
   headers: {
     Accept: 'application/vnd.twitchtv.v5+json',
     "Client-Id": `38kie46ro8k5h6crb314f7xylbncti`
   },
 }).then((response) => {//Full JSON Payload
-  console.log("response", response)
   let result = response.data.clips; //Array of Clip Objects
   console.log("result", result);
 
@@ -41,14 +40,14 @@ function getClips () {
 
   let profpic = document.querySelector("#profileImage");
   profpic.setAttribute("src", result[0].broadcaster.logo)
-//CURRENTLY SETTING UP FOR SECOND AXIOS CALL TO GRAB FOLLOWAGE
 
-  // let followage = document.getElementById('Followers');
-  // followage.innerHTML += 
 
   let counter = 0;
   result.forEach(element => {
-    console.log(element.url);
+    let thumbnail = result[counter].thumbnails.medium
+    // replacing -preview-480x272.jpg with .mp4
+    let thumbFormat = thumbnail.replace('-preview-480x272.jpg', '.mp4')
+    downloadLinks.push(thumbFormat)
     let clipList = document.querySelector('#videoField')
     let newClip = document.createElement('iframe')
     newClip.setAttribute("src", `${result[counter].embed_url}&parent=festive-dubinsky-21360a.netlify.app` )
@@ -56,5 +55,14 @@ function getClips () {
     counter += 1;
   });
 })};
-
 getClips();
+
+window.onload = function(){
+  let button = document.querySelector('#downloadButton')
+  button.addEventListener('click', downloadClips);
+}
+function downloadClips() {
+  downloadLinks.forEach(link => {
+    window.open(link);
+  })
+}
--- components/Header.js
+++ components/Header.js
@@ -17,6 +17,14 @@ export default st => html`
                     </ul>
                 </div>
             </div>
-        </div>`
+        </div>
+        <div id="searchBar">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-        //if this isn't working check out step 10 in 6.3 SPA
+            <!-- The form -->
+            <form class="example" action="action_page.php">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+        `
